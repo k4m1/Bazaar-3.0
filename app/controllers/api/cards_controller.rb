d540a2f20966c2b755cbebf487bcf376350a5650
Zak Johnson <fleecemane.lyons@icloud.com>
2019-11-07 17:21:13 -0800

    wrote seeding script and centered css

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,8 +1,13 @@
 class Api::CardsController < ApplicationController
   def index
+    @cards = Card.all
+    render :index
   end
 
   def show
+    @card = Card.find_by(params[:id])
   end
+
+  params.require(:card).permit(:rarity, :collector_number, :type_line, :printed_text, :name, :set_name, :image_uris, :flavor_text, :tcg_player_id)
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+card_cols = Card.column_names
+
+data = JSON.parse(File.read("sample_seeds.rb"))
+
+data.each do |card_datum|
+    card = Card.new
+    card_datum.keys.each do |key|
+        card_cols.each do |col|
+            card.send("#{col}=", card_data[key])
+        end
+    end
+    card.save!
+end

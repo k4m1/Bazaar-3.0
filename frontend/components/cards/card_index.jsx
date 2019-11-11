54aadb9b0c1141a1122f31b72badccc6ef29a856
Zak Johnson <fleecemane.lyons@icloud.com>
2019-11-11 08:48:26 -0800

    added and simple styled banner

--- frontend/components/cards/card_index.jsx
+++ frontend/components/cards/card_index.jsx
@@ -39,6 +39,20 @@ class CardIndex extends React.Component {
         let display; 
             if ( this.props.cards ) {
                 display = (
+
+                <div>
+
+                    <div className='banner-container'>
+                        <div className='banner-content'>
+                            <div className='banner-overlay'>
+                                <div className='banner-subhead'>Magic: the Gathering</div>
+                                <div className='banner-title'>Throne of Eldraine</div>
+                                <button className='banner-button'>Explore Now</button>
+                            </div>
+                                <img className="banner-image" src="https://tcgplayer-marketing.s3.amazonaws.com/web/release/2019-10/magic_throne_of_eldraine_hero_1920x590_10042019.jpg" alt=""/>
+                        </div>
+                    </div>
+
                     <div className="cataloge-wrapper">
                         
                             <div className="index-header-container">
@@ -70,47 +84,53 @@ class CardIndex extends React.Component {
                                 </section>
                             </div>
 
-                        <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-                        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+                            <div className='card-carousel'>
 
-                        <Slider {...settingsCarousel} className="card-carousel">
-                            <div>
-                                <h3>1</h3>
-                            </div>
-                            <div>
-                                <h3>2</h3>
-                            </div>
-                            <div>
-                                <h3>3</h3>
-                            </div>
-                            <div>
-                                <h3>4</h3>
-                            </div>
-                            <div>
-                                <h3>5</h3>
-                            </div>
-                            <div>
-                                <h3>6</h3>
-                            </div>
-                            <div>
-                                <h3>7</h3>
-                            </div>
-                            <div>
-                                <h3>8</h3>
-                            </div>
-                            <div>
-                                <h3>9</h3>
-                            </div>
-                            <div>
-                                <h3>10</h3>
-                            </div>
-                            <div>
-                                <h3>11</h3>
-                            </div>
-                            <div>
-                                <h3>12</h3>
-                            </div>
-                        </Slider>
+                                <h1 className='carousel-title'>Featured Cards</h1>
+
+                                <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+                                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+
+                                <Slider {...settingsCarousel} className="">
+
+                                    <div>
+                                        <h3>1</h3>
+                                    </div>
+                                    <div>
+                                        <h3>2</h3>
+                                    </div>
+                                    <div>
+                                        <h3>3</h3>
+                                    </div>
+                                    <div>
+                                        <h3>4</h3>
+                                    </div>
+                                    <div>
+                                        <h3>5</h3>
+                                    </div>
+                                    <div>
+                                        <h3>6</h3>
+                                    </div>
+                                    <div>
+                                        <h3>7</h3>
+                                    </div>
+                                    <div>
+                                        <h3>8</h3>
+                                    </div>
+                                    <div>
+                                        <h3>9</h3>
+                                    </div>
+                                    <div>
+                                        <h3>10</h3>
+                                    </div>
+                                    <div>
+                                        <h3>11</h3>
+                                    </div>
+                                    <div>
+                                        <h3>12</h3>
+                                    </div>
+                                </Slider>
+                        </div>
 
                         <div className="cataloge-content">
                         {this.props.cards.map(card => (
@@ -121,6 +141,7 @@ class CardIndex extends React.Component {
                         ))}
                         </div>
                     </div>
+                </div>
                 )
             } else {
                 display = null
